52a7fbc9176a7ce91c47d146cd2dbdf509f18ed3
batmacumba <brunocarneirodacunha@usp.br>
2022-05-21 18:45:27 -0300

    sync

--- MissionManager.py
+++ MissionManager.py
@@ -12,13 +12,16 @@ class MissionManager:
 
     def __init__(self, gamePath):
         self.gamePath = gamePath
-        self.mrParser =  MissionReportParser(self.gamePath, self.test)
+        self.mrParser =  MissionReportParser(self.gamePath, self.flagHandler)
         self.mrParser.run()
 
     # TODO: import modules according to mission name
     # TODO: erase method below
-    def test(missionReport):
+    def flagHandler(missionReport):
         print("flagHandler: " + missionReport)
+
+    def handleMissionChange(self, missionPath):
+        print('handleMissionChange: ' + missionPath)
 
 
 missionManager = MissionManager('C:\\Program Files (x86)\\Steam\\steamapps\\common\\IL-2 Sturmovik Battle of Stalingrad')
